7cbbdab926cba6f4fa70b7a59caa69fb957fad41
Abdul Rafay <65040443+abdrafay@users.noreply.github.com>
2021-03-09 19:29:55 +0500

    Fethcing Data using Redux

---
+++ frontend/src/Screens/CartScreen.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import {
+  Col,
+  Row,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
+import Message from "../Components/Message";
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+  return <div></div>;
+};
+
+export default CartScreen;
--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect } from "react";
 import Product from "../Components/Product";
-import {useDispatch, useSelector} from 'react-redux'
-import axios from "axios";
+import Message from "../Components/Message";
+import Loader from "../Components/Loader";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
-import {listProducts} from '../actions/productActions'
+import { listProducts } from "../actions/productActions";
+
 const HomeScreen = () => {
-  const dispatch = useDispatch()
-  const productList = useSelector(state => state.productList)
-  const {loading, error, products} = productList
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   // Getting Products From Backend
   useEffect(() => {
-    dispatch(listProducts())
+    dispatch(listProducts());
   }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading...</h2>: error ? <h3>{error}</h3>:<Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row> }
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
