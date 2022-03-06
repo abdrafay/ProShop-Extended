1335867f43a2da7b14ebe33d48c46ea53b082bc5
abdrafay <zainrafay12@gmail.com>
2022-03-06 12:05:14 +0500

    Shipping Screen Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import ProductScreen from "./Screens/ProductScreen";
 import CartScreen from "./Screens/CartScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
+import ShippingScreen from "./Screens/ShippingScreen";
+
 
 
 
@@ -18,8 +21,10 @@ const App = () => {
         <Header />
         <main className="py-3">
           <Container>
+            <Route path="/shipping" component={ShippingScreen} />
             <Route path="/login" component={LoginScreen} />
             <Route path="/register" component={RegisterScreen} />
+            <Route path="/profile" component={ProfileScreen} />
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/" component={HomeScreen} exact />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,25 +7,32 @@ import {
   productListReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
+import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from "./reducers/userReducers";
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
-  userRegister: userRegisterReducer
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+  : []
 const userInfoFromStorage = localStorage.getItem("userInfo")
 ? JSON.parse(localStorage.getItem("userInfo"))
-: null;
+: null
+
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+? JSON.parse(localStorage.getItem("shippingAddress"))
+: {}
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage
   },
   userLogin: {
     userInfo: userInfoFromStorage
